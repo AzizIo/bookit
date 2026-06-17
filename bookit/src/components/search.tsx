076becaffba1931f483179e88508e57d897b13fa
AzizIo <azizbekmadzitov@gmail.com>
2026-06-17 10:29:19 +0300

    Вынес секцию поиска в отедльные компонент чтобы избежать ререндера

--- bookit/src/components/search.tsx
+++ bookit/src/components/search.tsx
@@ -1,27 +1,44 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import ShimmerButton from '../components/ShimmerButton'
 
-export default function Search() {
-	const [query, setQuery] = useState('')
-	const navigate = useNavigate()
 
-	function handleSearch(e: React.FormEvent) {
-		e.preventDefault()
-		if (query.trim()) {
-			navigate(`/booking?q=${encodeURIComponent(query.trim())}`)
-			setQuery('')
-		}
-	}
+import { motion } from 'framer-motion'
+export default function SearchBar() {
+
+	const [searchTerm, setSearchTerm] = useState('')
+
 
 	return (
-		<form onSubmit={handleSearch} className="search-section">
-			<input
-				type="text"
-				value={query}
-				onChange={(e) => setQuery(e.target.value)}
-				placeholder='Поиск...'
-				className='rounded-xl px-4 py-2 border-1 border-gray-600 font-semibold text-[#ced0d3] bg-[#2a3147] max-w-md w-120'
-			/>
-		</form>
+		<motion.form
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ delay: 0.5, duration: 0.5 }}
+			className='w-full mb-20'
+		>
+			<div className="bg-white/5 mx-4 md:mx-auto border border-gray-700 rounded-xl px-3 py-3 md:px-4 md:py-4 flex flex-col gap-4 items-center max-w-3xl">
+				<div className="flex flex-col md:flex-row gap-3 w-full justify-between items-center">
+					<div className="search flex w-full md:w-auto">
+						<input type="text" onChange={e => setSearchTerm(e.target.value)}
+							value={searchTerm}
+							placeholder='Местоположение' required className='rounded-xl px-4 py-3 border border-gray-600 font-semibold text-[#ced0d3] bg-[#2a3147] w-full md:w-60 h-16' />
+					</div>
+					<div className="date flex w-full md:w-auto">
+						<input type="date" className='rounded-xl px-4 py-3 border border-gray-600 font-semibold text-[#ced0d3] bg-[#2a3147] w-full md:w-60 h-16' />
+					</div>
+					<div className="sel flex w-full md:w-auto">
+						<select className='rounded-xl px-4 py-3 border border-gray-600 font-semibold text-[#ced0d3] bg-[#2a3147] w-full md:w-60 h-16' name="" id="">
+							<option value="">Категории</option>
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+						</select>
+					</div>
+				</div>
+				<div className="buttin flex justify-center  w-full">
+					<ShimmerButton className='rounded-xl px-8 py-3 text-lg border font-semibold text-black bg-[#f5a623] w-full'>Найти и забронировать</ShimmerButton>
+				</div>
+			</div>
+		</motion.form>
 	)
 }
--- bookit/src/pages/MainPage.tsx
+++ bookit/src/pages/MainPage.tsx
@@ -13,6 +13,7 @@ import GlowText from '../components/GlowText'
 import MagneticButton from '../components/MagneticButton'
 import ShimmerButton from '../components/ShimmerButton'
 import TextReveal from '../components/TextReveal'
+import SearchBar from '../components/search'
 
 const featuredReview = {
 	name: 'Sarah Chen',
@@ -38,7 +39,7 @@ export default function MainPage() {
 	}
 	const [searchTerm, setSearchTerm] = useState('')
 	const [AllListings, setAllListings] = useState<Listing[]>([])
-	const [featuredReviewData, setFeaturedReviewData] = useState<{name: string; rating: number; text: string; role: string} | null>(null)
+	const [featuredReviewData, setFeaturedReviewData] = useState<{ name: string; rating: number; text: string; role: string } | null>(null)
 	useEffect(() => {
 		API.get('/listings/three').then((res) => setAllListings(res.data))
 		API.get('/reviews/featured')
@@ -88,36 +89,7 @@ export default function MainPage() {
 					</motion.div>
 				</section>
 				{/* Секция поиска */}
-				<motion.form
-					initial={{ opacity: 0, y: 20 }}
-					animate={{ opacity: 1, y: 0 }}
-					transition={{ delay: 0.5, duration: 0.5 }}
-					className='w-full mb-20'
-				>
-					<div className="bg-white/5 mx-4 md:mx-auto border border-gray-700 rounded-xl px-3 py-3 md:px-4 md:py-4 flex flex-col gap-4 items-center max-w-3xl">
-						<div className="flex flex-col md:flex-row gap-3 w-full justify-between items-center">
-							<div className="search flex w-full md:w-auto">
-								<input type="text" onChange={e => setSearchTerm(e.target.value)}
-									value={searchTerm}
-									placeholder='Местоположение' required className='rounded-xl px-4 py-3 border border-gray-600 font-semibold text-[#ced0d3] bg-[#2a3147] w-full md:w-60 h-16' />
-							</div>
-							<div className="date flex w-full md:w-auto">
-								<input type="date" className='rounded-xl px-4 py-3 border border-gray-600 font-semibold text-[#ced0d3] bg-[#2a3147] w-full md:w-60 h-16' />
-							</div>
-							<div className="sel flex w-full md:w-auto">
-								<select className='rounded-xl px-4 py-3 border border-gray-600 font-semibold text-[#ced0d3] bg-[#2a3147] w-full md:w-60 h-16' name="" id="">
-									<option value="">Категории</option>
-									<option value="1">1</option>
-									<option value="2">2</option>
-									<option value="3">3</option>
-								</select>
-							</div>
-						</div>
-						<div className="buttin flex justify-center  w-full">
-							<ShimmerButton className='rounded-xl px-8 py-3 text-lg border font-semibold text-black bg-[#f5a623] w-full'>Найти и забронировать</ShimmerButton>
-						</div>
-					</div>
-				</motion.form>
+				<SearchBar />
 
 				{/* Статистика */}
 				<section className="py-16 border-y border-white/5">
@@ -183,11 +155,11 @@ export default function MainPage() {
 											<span>📍</span>
 											<span>{l.city}</span>
 										</div>
-									<div className="flex items-center gap-2 text-[#8b93a8] text-xs">
-										<span className="text-yellow-400">★</span>
-										<span className="text-white font-semibold">{(l.average_rating ?? 0).toFixed(1)}</span>
-										<span>({l.reviews_count ?? 0})</span>
-									</div>
+										<div className="flex items-center gap-2 text-[#8b93a8] text-xs">
+											<span className="text-yellow-400">★</span>
+											<span className="text-white font-semibold">{(l.average_rating ?? 0).toFixed(1)}</span>
+											<span>({l.reviews_count ?? 0})</span>
+										</div>
 										<div className="flex items-center gap-1 text-xds">
 											<span className="text-[#f5a623]">🏷️</span>
 											<span className="text-[#f5a623] font-semibold">{l.category}</span>
